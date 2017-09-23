a525f8852f82af5adeff6c2b224dee3ce74b1e29
margaretmcrawf <margaretmcrawf@gmail.com>
2017-09-23 19:30:54 -0400

    :)

--- infrared_sensor/infrared_sensor.ino
+++ infrared_sensor/infrared_sensor.ino
@@ -2,7 +2,8 @@
 #include <Servo.h>
 
 //servo object to control the servo
-Servo myservo;
+Servo xServo;
+Servo zServo;
 
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
@@ -12,7 +13,8 @@ int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 int sensorMin = 1023;
 int sensorMax = 0;
-int pos = 0;
+int posX = 50;
+int posZ = 30;
 
 int timeOutput = millis();
 
@@ -24,68 +26,69 @@ float dist(int sensorVal) {
   return -0.0397*sensorVal + 28.93;
 }
 
+/*
+ * Get the average of 10 sensor readings, to account for occasional
+ * inconsistencies in readings
+ */
+float average_distance_val(int reps) {
+  float total = 0;
+  for (int i = 0; i < reps; i++) {
+    // read the analog in value:
+    int tempSensorValue = analogRead(analogInPin);
+    // map it to a distance
+    float distance = dist(sensorValue);
+    //add it to the total for averaging purposes
+    total += distance;
+    sensorValue = tempSensorValue;
+    
+    // wait 2 milliseconds before the next loop for the analog-to-digital
+    // converter to settle after the last reading:
+    delay(50);
+  }
+  return total/reps;
+}
+
+char print_string(float dist, int pos_x, int pos_z) {
+  Serial.print("distance = ");
+  Serial.print(dist);
+  Serial.print(" angle1 = ");
+  Serial.print(pos_x);
+  Serial.print(" angle2 = ");
+  Serial.println(pos_z);
+}
+
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  myservo.attach(9);
+  xServo.attach(9);
+  zServo.attach(10);
   //digitalWrite(analogOutPin, LOW);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) {
-      //move the servo
-      myservo.write(pos);
-      delay(100);
-      float total = 0;
-      for (int i = 0; i < 10; i++) {
-        // read the analog in value:
-        int tempSensorValue = analogRead(analogInPin);
-        // map it to a distance
-        float distance = dist(sensorValue);
-        //add it to the total for averaging purposes
-        total += distance;
-        sensorValue = tempSensorValue;
-        
-        // wait 2 milliseconds before the next loop for the analog-to-digital
-        // converter to settle after the last reading:
-        delay(20);
-      }
-      //get the average of the 10 readings
-      float distAverage = total / 10.0;
-      //print to serial
-//      Serial.print("sensor = ");
-//      Serial.println(sensorValue);
-      Serial.print(" distance = ");
-      Serial.print(distAverage);
-      Serial.print(" angle = ");
-      Serial.println(pos);
-  }
-
-  for (pos = 180; pos >= 0; pos -= 1) {
-      //move the servo
-      myservo.write(pos);
-      delay(100);
-      float total = 0;
-      for (int i = 0; i < 10; i++) {
-        // read the analog in value:
-        int tempSensorValue = analogRead(analogInPin);
-        // map it to a distance
-        float distance = dist(sensorValue);
-        sensorValue = tempSensorValue;
-
-        // wait 2 milliseconds before the next loop for the analog-to-digital
-        // converter to settle after the last reading:
-        delay(2);
-      }
-      //get the average of the 10 readings
-      float distAverage = total / 10.0;
-      // print the results to the Serial Monitor:
-      Serial.print("sensor = ");
-      Serial.print(sensorValue);
-      Serial.print("distance = ");
-      Serial.print(distAverage);
-      Serial.print("Angle = ");
-      Serial.println(pos);
+  for (posZ = 60; posZ <= 120; posZ += 20) {
+    //move the servo in the Z direction
+    zServo.write(posZ);
+    for (posX = 30; posX <= 100; posX += 10) {
+        //move the servo
+        xServo.write(posX);
+        delay(100);
+        //get the average distance from 10 sensor readings
+        float distAverage = average_distance_val(10);
+        //print to serial
+        print_string(distAverage, posX, posZ);
+    }
+    //move the servo in the Z direction
+    zServo.write(posZ + 10);
+    for (posX = 100; posX >= 30; posX -= 10) {
+        //move the servo
+        xServo.write(posX);
+        delay(100);
+        //get the average distance from 10 sensor readings
+        float distAverage = average_distance_val(10);
+        // print the results to the Serial Monitor:
+        print_string(distAverage, posX, posZ + 10);
+    }
   }
 
 }
--- read_serial.py
+++ read_serial.py
@@ -1,24 +1,64 @@
 from serial import Serial, SerialException
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+
+ANGLE_MIN = 30
+ANGLE_MAX = 100
 
 cxn = Serial('/dev/ttyACM0', baudrate=9600)
 
-x = y = np.zeros((181))
-X, Y = np.meshgrid(x, y)
+xs = np.zeros((19*19, 1))
+ys = np.zeros((19*19, 1))
+zs = np.zeros((19*19, 1))
 
+y = 2 # offset in inches, fake data
+
+def convert_to_cartesian(phi, beta, distance):
+    theta = 90 - phi
+    z = y / math.cos(math.radians(theta))
+    print(z)
+    x = z*math.sin(math.radians(theta))
+    t = distance - x
+    l = t*math.sin(math.radians(phi))
+    yPos = z + l
+    xPos = t * math.cos(math.radians(phi))
+    return xPos, yPos, 0
+
+i = 0
 while True:
     while cxn.inWaiting() < 1:
         pass
     result = cxn.readline()
-    if "angle = " in result.decode() and "distance" in result.decode():
-        angle = result.decode().split(" ")[6]
-        angle = int(angle)
-        dist = result.decode().split(" ")[3] 
+    #print(result)
+    if "angle1 = " in result.decode() and "distance" in result.decode():
+        angle1= result.decode().split(" ")[5]
+        angle1 = int(angle1)
+        angle2 = result.decode().split(" ")[8]
+        angle2 = int(angle2)
+        dist = result.decode().split(" ")[2] 
         dist = float(dist)
-        print(angle, dist)
-        for i in range(180):
-            X[i, angle] = dist
-        if angle == 180:
-            plt.contourf(X)
+        #print(angle1, angle2, dist)
+        
+        x, y, z = convert_to_cartesian(angle1, angle2, dist)
+        xs[i] = x
+        ys[i] = y
+        zs[i] = z
+        i += 1
+        print(angle1, angle2, dist, x, y, z)
+
+        #X[angle2//10, angle1//10] = dist
+        if angle2 >= ANGLE_MAX and angle1 >= ANGLE_MAX:
+            #plt.contourf(X)
+            #plt.show()
+            i = 0
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(xs, ys, zs)
             plt.show()
+
+        if angle2 <= ANGLE_MIN and angle1 <= ANGLE_MIN:
+            i = 0
+
+ 
